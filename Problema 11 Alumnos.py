36800291ac78e9a43c036cbf1a6d5a80b0ef917d
Desmadroso <100376597+Desmadroso@users.noreply.github.com>
2022-03-05 15:13:52 -0700

    Add files via upload

---
+++ Problema 11 Alumnos.py
@@ -0,0 +1,28 @@
+import sys
+print ("Inserta cuantos alumnos son ")
+p=int(input())
+lista = []
+print ("Inserta las matriculas de los alumnos")
+for i in range(0,p):
+        n=int(input())
+        lista.append(n)
+print ("La lista es: ")
+for i in range(0,len(lista)):
+    print (lista[i])
+def particionado (lista):
+    pivote = lista[0]
+    menores=[]
+    mayores=[]
+    
+    for i in range(1,len(lista)):
+        if lista[i]<pivote:
+            menores.append(lista[i])
+        else :
+            mayores.append(lista[i])
+    return menores, pivote, mayores
+def quicksort(lista):
+    if len(lista)<2:
+        return lista
+    menores,pivote,mayores = particionado(lista)
+    return quicksort(menores) + [pivote] + quicksort(mayores)
+print(quicksort(lista))
